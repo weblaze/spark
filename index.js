75a4b3cea486e0e6bdacf9c4ff393283abf2d125
Aditya Swami <86168331+adityaswamii@users.noreply.github.com>
2021-07-01 21:09:49 +0530

    Contains: navbar, carousel, column sections, modal popup

---
+++ index.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
+$(document).ready(function(){
+  $('.carousel').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+});
+
+/** REGISTRATION FORM (MODAL) */
+
+const registerButton = document.querySelector('#Register');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+registerButton.addEventListener('click', () => {
+  modal.classList.add('is-active');
+});
+
+modalBg.addEventListener('click', () => {
+  modal.classList.remove('is-active');
+});
